70f1494f0d26d25a48c17b66d1f96b9bbec667ef
muxiamu <131332438+muxiamu@users.noreply.github.com>
2024-01-21 21:50:16 +0800

    Create LED_1&2.c
    两个LED灯亮灭状态相反

---
+++ LED_1&2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<wiringPi.h>
+#include<stdlib.h>
+
+# LED管脚
+#define LEDpin1 15
+#define LEDpin2 16
+
+
+
+int main(void)
+{
+    unsigned int i;
+    if (wiringPiSetup() == -1)
+    {
+        printf("setup error\n");
+        exit(-1);
+    }
+
+    pinMode(LEDpin1,OUTPUT);
+    pinMode(LEDpin2,OUTPUT);
+
+    while(1){
+    digitalWrite(LEDpin1,HIGH);
+    digitalWrite(LEDpin2,LOW);
+    delay(100);
+    digitalWrite(LEDpin1,LOW);
+    digitalWrite(LEDpin2,HIGH);
+    delay(100);
+}
+}
